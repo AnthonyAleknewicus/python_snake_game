61171433e8774bea4ab45d3fa06493eebf2c74b1
AnthonyAleknewicus <anthonyaleknewicus@gmail.com>
2025-04-02 10:43:37 -0700

    added tail collision and game over

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ screen.onkey(key="Right", fun=snake.right)
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.09)
     snake.move()
 
     if snake.head.distance(food) < 20:
@@ -38,6 +38,14 @@ while game_is_on:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 
